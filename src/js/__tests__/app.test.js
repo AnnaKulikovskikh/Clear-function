355b4dd1753cdb610490f268957f2dd053d45cde
Anna <k_anna_i@mail.ru>
2021-08-19 20:52:38 +0300

    190821

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,18 +1,10 @@
 import healthMagic from '../app';
 
-test('health', () => {
-  //const hero = {name: 'magic', health: 51};
-  expect(healthMagic({ name: 'magic', health: 51 })).toBe('healthy');
-  expect(healthMagic({ name: 'magic', health: 50 })).toBe('wounded');
-  expect(healthMagic({ name: 'magic', health: 15 })).toBe('critical');
+test.each([
+  [51, 'healthy'],
+  [50, 'wounded'],
+  [15, 'critical'],
+])('health-status', (points, expected) => {
+  const hero = { name: 'magic', health: points };
+  expect(healthMagic(hero)).toBe(expected);
 });
-
-//почему-то не работет. Пишет Cannot read property 'length' of undefined. Почему не поняла.
-// test.each([
-//     [51, 'healthy' ],
-//     [50, 'wounded'],
-//     [15, 'critical'],
-// ])('health-status'),(points, expected) => {
-//     const hero = {name: 'magic', health: points};
-//     excpect(healthMagic(hero)).toBe(excpected);
-// }
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,7 @@
 // TODO: write your code here
-import sum from './basic';
-console.log('worked');
-console.log(sum([1, 2]));
 
 export default function health(hero) {
-    if (hero.health > 50) return 'healthy';
-    if (hero.health > 15) return 'wounded';
-    return 'critical';
+  if (hero.health > 50) return 'healthy';
+  if (hero.health > 15) return 'wounded';
+  return 'critical';
 }
